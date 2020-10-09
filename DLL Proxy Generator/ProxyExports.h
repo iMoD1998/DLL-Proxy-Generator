64b139a68a9cdbc05ed6ea8c6ea938d9953b6a67
iMoD1998 <imod1998@outlook.com>
2020-10-09 23:58:38 +0100

    Remove generated testing stuff

--- DLL Proxy Generator/ProxyExports.h
+++
@@ -1,6 +0,0 @@
-#pragma comment(linker,"/export:#36=ntdll.#36,@36,NONAME")
-#pragma comment(linker,"/export:HeapAlloc=ntdll.RtlAllocateHeap")
-#pragma comment(linker,"/export:ImAGayFunction=Proxy.ImAGayFunction")
-#pragma comment(linker,"/export:Lmao=Proxy.Lmao,DATA")
-#pragma comment(linker,"/export:#234=ntdll.#234,@234,NONAME")
-#pragma comment(linker,"/export:#456=ntdll.#456,@456,NONAME")
